f41db6f7f9aaad371c23e1f67d3e598f867fa1e5
Constantin Egloff <constantinegloff03@gmail.com>
2023-09-29 11:30:19 +0200

    App-logo bugged, background centered

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import '../styles/App.css';
+import logo from '../assets/logo-rick-et-morty.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <div className='App-logo'>
+          <img src='../assets/logo-rick-et-morty.png'></img>
+        </div>
       </header>
     </div>
   );
