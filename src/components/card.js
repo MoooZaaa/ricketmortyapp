a6a58025f343c382bfc303c667096fe575b5be1d
boubalourson <boobaloursonZEBI@gmail.com>
2023-09-29 14:13:07 +0200

    Card ça marche

--- src/components/card.js
+++ src/components/card.js
@@ -1,16 +1,17 @@
-import React from 'react'
+// import React from 'react'
+// import TestComponents from './test'
 
-function Card() {
-  return (
-    <div class="card" style={{width: 18 + 'em'}}>
-        <img src="..." class="card-img-top" alt="..."/>
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-    </div>
-  )
-}
+// function Card() {
+//   return (
+//     <div class="card" style={{width: 18 + 'em'}}>
+//         <img src="" class="card-img-top" alt="..."/>
+//         <div class="card-body">
+//             <h5 class="card-title"></h5>
+//             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//             <a href="#" class="btn btn-primary">Go somewhere</a>
+//         </div>
+//     </div>
+//   )
+// }
 
-export default Card
+// export default Card
--- src/components/test.js
+++ src/components/test.js
@@ -26,10 +26,15 @@ function TestComponents() {
     return <div>Chargement...</div>;
   } else {
     return (
-      <div class="text-danger">
-        <p>Name : {character.name}</p>
+    <div class="card" style={{width: 18 + 'em'}}>
+      <img src={character.image} class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">{character.name}</h5>
+        <p class="card-text">{character.location.name}</p>
+        <a href="#" class="btn btn-dark">Go somewhere</a>
       </div>
-    );
+    </div>
+    )
   }
 }
 
