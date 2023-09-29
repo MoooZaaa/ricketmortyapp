5d8f022bec9cfec6ad27dbd66833486d93792631
Constantin Egloff <constantinegloff03@gmail.com>
2023-09-29 11:44:34 +0200

    Test working

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import '../styles/App.css';
+import TestComponents from './test';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <TestComponents/>
       </header>
     </div>
   );
