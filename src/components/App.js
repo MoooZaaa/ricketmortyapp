630efe277f69e350d24a0b5d96a9517b85adf44c
boubalourson <boobaloursonZEBI@gmail.com>
2023-09-29 13:49:08 +0200

    Change card for bootstrap card

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import '../styles/App.css';
 import TestComponents from './test';
-
+import Card from './card';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <TestComponents/>
+        {Card()}
       </header>
     </div>
   );
--- src/components/card.js
+++ src/components/card.js
@@ -2,32 +2,12 @@ import React from 'react'
 
 function Card() {
   return (
-    <div class="card">
-        <div class="card-image">
-            <figure class="image is-4by3">
-            <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
-            </figure>
-        </div>
-        <div class="card-content">
-            <div class="media">
-                <div class="media-left">
-                    <figure class="image is-48x48">
-                    <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image"/>
-                    </figure>
-                </div>
-                <div class="media-content">
-                    <p class="title is-4">John Smith</p>
-                    <p class="subtitle is-6">@johnsmith</p>
-                </div>
-                </div>
-
-                <div class="content">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-                <a href="#">#css</a> <a href="#">#responsive</a>
-                <br/>
-                <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-            </div>
+    <div class="card" style={{width: 18 + 'em'}}>
+        <img src="..." class="card-img-top" alt="..."/>
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
     </div>
   )
